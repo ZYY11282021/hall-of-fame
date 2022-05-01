069bc589bae9e3bdd6b8176e23471decd9b25abf
ZYY <95468604+ZYY11282021@users.noreply.github.com>
2022-05-01 11:40:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 console.log("欢迎来到可怕的世界名人堂！");
 console.log("%c招聘：UI设计师 站长QQ号：692309290", "color: black; background-color: #7fffaa;");
+console.log("%c捐赠：本网站从2021年10月一路走到现在 制作过程比较困难 如果您有兴趣的话可以考虑捐赠我们 站长微信号：happy_yang11 非常感谢！", "color: skyblue;");
